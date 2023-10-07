0023b1f4f85464255f58048b785c8880dead8158
Maleen Ashinshana <maleenashinshana70@gmail.com>
2023-10-08 00:52:29 +0530

    create the admin login page

---
+++ Js/style.js
@@ -0,0 +1,7 @@
+
+document.getElementById("admin-navLink").addEventListener("click",function (){
+    $('#admin-login').css({display:"block",position:'relative',top:'0%'})
+    /*$('#main').css({position:'absolute',leaf:'38%',justifyContent:'center',alignItems:'center'})*/
+    $('#firstPage').css({display:"none"})
+})
+
