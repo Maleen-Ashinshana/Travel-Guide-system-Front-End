26eb253b0c1dae4d93cc2869a3f65ee8b3e7794a
Maleen Ashinshana <maleenashinshana70@gmail.com>
2023-10-19 03:00:21 +0530

    create user register form

--- Js/style.js
+++ Js/style.js
@@ -1,11 +1,22 @@
-
+$('#user_login').css({display:"none"})
 document.getElementById("admin-navLink").addEventListener("click",function (){
     $('#admin-login').css({display:"block",position:'relative',top:'0%'})
     /*$('#main').css({position:'absolute',leaf:'38%',justifyContent:'center',alignItems:'center'})*/
     $('#firstPage').css({display:"none"})
+    // $('#div2').css({display:"none"})
+    $('#user_login').css({display:"none"})
 })
 document.getElementById("btn-register").addEventListener('click',function (){
     $('#user_login').css({display:"block",position: "relative",top: "0%"})
     $('#firstPage').css({display:"none"})
+    $('#div2').css({display:"none"})
 })
-
+document.getElementById("btn-user-register").addEventListener('click',function (){
+    $('#div2').css({display:"block"})
+    $('#div1').css({display:"none"})
+})
+document.getElementById("btn-new-user-register").addEventListener('click',function (){
+    $('#div2').css({display:"none"})
+    $('#div1').css({display:"block"})
+    /*$('#user_login').css({display:"block"})*/
+})
