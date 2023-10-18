a8da382317fb2a513387f54026e9a010a27fa35a
Maleen Ashinshana <maleenashinshana70@gmail.com>
2023-10-18 19:45:00 +0530

    create user login page

--- Js/style.js
+++ Js/style.js
@@ -4,4 +4,8 @@ document.getElementById("admin-navLink").addEventListener("click",function (){
     /*$('#main').css({position:'absolute',leaf:'38%',justifyContent:'center',alignItems:'center'})*/
     $('#firstPage').css({display:"none"})
 })
+document.getElementById("btn-register").addEventListener('click',function (){
+    $('#user_login').css({display:"block",position: "relative",top: "0%"})
+    $('#firstPage').css({display:"none"})
+})
 
