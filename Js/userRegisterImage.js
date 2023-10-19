d6de77cb893b7ba915eff08e8324e99694c4013b
Maleen Ashinshana <maleenashinshana70@gmail.com>
2023-10-19 16:08:51 +0530

    add guider page

--- Js/userRegisterImage.js
+++ Js/userRegisterImage.js
@@ -2,3 +2,8 @@ var loadFile = function (event) {
     var image = document.getElementById("output");
     image.src = URL.createObjectURL(event.target.files[0]);
 };
+
+var loadFile = function (event) {
+    var image = document.getElementById("guide-output");
+    image.src = URL.createObjectURL(event.target.files[0]);
+};
