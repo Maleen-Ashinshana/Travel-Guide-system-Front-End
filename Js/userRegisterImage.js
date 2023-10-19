e59258620eccba29ca59a37e62a240ea1c36a1c7
Maleen Ashinshana <maleenashinshana70@gmail.com>
2023-10-19 23:02:57 +0530

    guide-admin page v1.1

--- Js/userRegisterImage.js
+++ Js/userRegisterImage.js
@@ -1,9 +1,33 @@
+/*var loadFile = function (event) {
+    var image = document.getElementById("output");
+    image.src = URL.createObjectURL(event.target.files[0]);
+};
+/!*function loadGuiderImage(){
+
+}*!/
+var loadImg = function (event) {
+    var img = document.getElementById("guide-output");
+    img.src = URL.createObjectURL(event.target.files[0]);
+};*/
+/*function loadImage(event, targetElementId) {
+    var element = document.getElementById(targetElementId);
+    element.src = URL.createObjectURL(event.target.files[0]);
+}
+
+var loadFile = function (event) {
+    loadImage(event, "output");
+};
+
+var loadImg = function (event) {
+    loadImage(event, "guide-output");
+};*/
+
 var loadFile = function (event) {
     var image = document.getElementById("output");
     image.src = URL.createObjectURL(event.target.files[0]);
 };
 
-var loadFile = function (event) {
-    var image = document.getElementById("guide-output");
-    image.src = URL.createObjectURL(event.target.files[0]);
-};
+/*var loadImg = function (event) {
+    var img = document.getElementById("guide-output");
+    img.src = URL.createObjectURL(event.target.files[0]);
+};*/
