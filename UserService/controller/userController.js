36977d1a1226e79f5f33ae50ccb4f2902481703d
Maleen Ashinshana <maleenashinshana70@gmail.com>
2023-10-22 19:13:52 +0530

    update all

--- UserService/controller/userController.js
+++ UserService/controller/userController.js
@@ -8,46 +8,6 @@ import {UserModel} from "../model/userModel.js";
 
 export class UserController{
     constructor() {
-
-        this.useiPic = $('#file').file[0];
-
-
-        /*const express = require('express');
-        const app = express();
-
-
-        app.use((req, res, next) => {
-            res.header('Access-Control-Allow-Origin', '*'); // '*' allows any origin
-            res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-            res.header('Access-Control-Allow-Headers', 'Content-Type');
-            next();
-        });
-    }*/
-
-       /* const express = require('express');
-        const cors = require('cors');
-        const app = express();*/
-
-// Configure CORS to allow requests from your front-end origin
-  /*      const allowedOrigins = ['http://localhost:63342/Proposed%20Travel%20Planning%20System%20Front%20End/index.html?_ijt=1logoag0jji8ocn8gg4iuahuu7&_ij_reload=RELOAD_ON_SAVE.com'];
-        const corsOptions = {
-            origin: function (origin, callback) {
-                if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-                    callback(null, true);
-                } else {
-                    callback(new Error('Not allowed by CORS'));
-                }
-            },
-        };*/
-
-        /*app.use(cors(corsOptions));*/
-
-// Your routes and server logic here
-
-        /*app.listen(3000, () => {
-            console.log('Server is running on port 3000');
-        });*/
-
     }
 
 }
@@ -55,43 +15,29 @@ export class UserController{
 document.getElementById('btn-new-user-register').addEventListener("click", function () {
 
 
-    let userName=$('#txt-user-name').val()
-    let userAddress=$('#txt-user-address').val()
-    let userEmail=$('#txt-user-email').val()
-    let userContact=$('#txt-user-contact').val()
-    let userPassword=$('#txt-user-password').val()
-    let userGender=$('#txt-user-gender').val()
-    let userNIC=$('#txt-user-nic').val()
-    let registerDate=$('#txt-user-date').val()
+    const userName=$('#txt-user-name').val()
+    const userAddress=$('#txt-user-address').val()
+    const userEmail=$('#txt-user-email').val()
+    const userContact=$('#txt-user-contact').val()
+    const userPassword=$('#txt-user-password').val()
+    const userGender=$('#txt-user-gender').val()
+    const userNIC=$('#txt-user-nic').val()
+    const registerDate=$('#txt-user-date').val()
+    const userProfile=$('#file')[0].files[0]
 
-     const userProfile=$('#file')[0].files[0]
-  /*  const registerDate = new Date();
-    console.log("Current date: " + registerDate);*/
+    const dto=new UserModel(userName,userAddress,userEmail,userPassword,userContact,userNIC,registerDate,userGender,/*userProfile*/);
 
 
-    const dto=new UserModel(userName,userAddress,userEmail,userPassword,userContact,userNIC,registerDate,userGender,userProfile);
-
-    /*const fromData=new FormData()
-    const jDTO=JSON.stringify(dto);
-    const blob=new Blob([jDTO],{type:'application/json'})
-
-    fromData.set("dto",blob);
-    fromData.set("file",pic);
-*/
     const formData = new FormData();
-    formData.append("dto", new Blob([JSON.stringify(dto)], { type: 'application/json' }));
+    formData.append("userModel", new Blob([JSON.stringify(dto)], { type: 'application/json' }));
     formData.append('userName',userName);
     formData.append('userAddress', userAddress);
-  /*  formData.append('registerDate', registeDate);*/
     formData.append('registerDate', registerDate);
-   /* formData.append('address',userAddress);*/
     formData.append('userEmail', userEmail);
     formData.append('userContact',userContact);
     formData.append('userPassword',userPassword);
     formData.append('userNIC',userNIC);
     formData.append('userGender',userGender);
-
-    /*formData.append("dto",dto)*/
     formData.append("userProfile", userProfile);
     $.ajax({
         type:"POST",
@@ -113,86 +59,7 @@ document.getElementById('btn-new-user-register').addEventListener("click", funct
         })
     })
 
-
-
-
-    /*let user_data = {
-        _name: $('#txt-user-name').val(),
-        _address: $('#txt-user-address').val(),
-        _email: $('#txt-user-email').val(),
-        _password: $('#txt-user-password').val(),
-        _contact: $('#txt-user-contact').val(),
-        _gender: $('#txt-user-gender').val(),
-    };
-    const fileInput = document.getElementById('file');
-    const file = fileInput.files[0];
-
-
-    console.log(user_data._name + "****")
-    console.log(user_data._address + "****")
-    console.log(user_data._email + "****")
-    console.log(user_data._password + "****")
-    console.log(user_data._contact + "****")
-    console.log(user_data._gender + "****")
-     /!*const profile=$('#output').files[0]*!/
-    const js = JSON.stringify(user_data);
-    const blob = new Blob([JSON], {
-        type: 'application/json'
-    });
-
-    /!*const img=*!/
-
-   const  formData=new FormData();
-   formData.append("user_data",blob);
-   formData.append("file",file);
-
-    $.ajax({
-        type: "POST",
-        url: "http://localhost:9095/User/api/v1/user",
-        headers: {
-            'content-type':
-        },
-        dataType: "json",
-        success: (res) => {
-            console.log(res);
-        },
-        error: (error) => {
-            // window.location.href = '../Login/index.html';
-        }
-    });*/
 })
-
-
-/*const dto = new UserDTO(username, password, email, nicPassportNumber, address, phoneNumber, userType, userGender, remarks, dob);
-        const formData = new FormData();
-        const jsonDTO = JSON.stringify(dto);
-        const blob = new Blob([jsonDTO], {type: 'application/json'});
-
-
-        formData.set("userDTO", blob);
-        formData.set("profilePicture", profilePicture);
-        formData.set("nicPassportFrontImg", nicPassportFront);
-        formData.set("nicPassportBackImg", nicPassportBack);
-
-        $.ajax({
-            type: "POST",
-            url: this.adminRegisterApi,
-            data: formData,
-            processData: false,
-            contentType: false,
-            headers: {
-                "Authorization": "Bearer "+ this.token
-            },
-            success: (response) => {
-                console.log("Guide saved successfully. User ID: " + response.data);
-                this.clearInputFields();
-                this.getAllGuides();
-                $("#imageIdFrontSection, #imageIdBackSection, #guideImageSection").hide();
-            },
-            error: (error) => {
-                console.error("Error saving guide: " + error.responseText);
-            }
-        });*/
 
 
 
--- UserService/model/userModel.js
+++ UserService/model/userModel.js
@@ -1,96 +1,16 @@
 export class UserModel{
+    constructor(userName,userAddress,userEmail,userPassword,userContact,userNIC,registerDate,userGender,userProfile) {
 
-    constructor(name,address,registerDate,email,password,contact,gender,nic) {
-        this._id = null;
-        this._name = name;
-        this._address = address;
-        this._registerDate = registerDate;
-        this._email = email;
-        this._password = password;
-        this._contact = contact;
-        this._gender = gender;
-        this._nic = nic;
-        this._profile = null;
+        this.userName = userName;
+        this.userAddress = userAddress;
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+        this.userContact = userContact;
+        this.userNIC = userNIC;
+        this.registerDate = registerDate;
+        this.userGender = userGender;
+        this.userProfile = userProfile;
     }
 
 
-    get id() {
-        return this._id;
-    }
-
-    set id(value) {
-        this._id = value;
-    }
-
-    get name() {
-        return this._name;
-    }
-
-    set name(value) {
-        this._name = value;
-    }
-
-    get address() {
-        return this._address;
-    }
-
-    set address(value) {
-        this._address = value;
-    }
-
-    get registerDate() {
-        return this._registerDate;
-    }
-
-    set registerDate(value) {
-        this._registerDate = value;
-    }
-
-    get email() {
-        return this._email;
-    }
-
-    set email(value) {
-        this._email = value;
-    }
-
-    get password() {
-        return this._password;
-    }
-
-    set password(value) {
-        this._password = value;
-    }
-
-    get contact() {
-        return this._contact;
-    }
-
-    set contact(value) {
-        this._contact = value;
-    }
-
-    get gender() {
-        return this._gender;
-    }
-
-    set gender(value) {
-        this._gender = value;
-    }
-
-    get nic() {
-        return this._nic;
-    }
-
-    set nic(value) {
-        this._nic = value;
-    }
-
-    get profile() {
-        return this._profile;
-    }
-
-    set profile(value) {
-        this._profile = value;
-    }
 }
