c87628ee3ce6e88f91e35c908888e097c741fafb
Maleen Ashinshana <maleenashinshana70@gmail.com>
2023-10-23 19:31:59 +0530

    create vehicle pane

--- Js/addBluerrPage.js
+++ Js/addBluerrPage.js
@@ -1,8 +1,12 @@
 const openButton=document.getElementById("btn-add-guider-page")
+const vehiclePaneOpenButton=document.getElementById("btn-add-vehicle-page")
 /*const guideUpdateOpenButton=document.getElementById("btn-guide-pane")*/
 const closeButton = document.getElementById("closeButton");
 const overlay = document.getElementById("overlay");
 const newPage = document.getElementById("newPage");
+const vehicleOverlay = document.getElementById("vehicleOverlay");
+const newVehiclePage = document.getElementById("newVehiclePage");
+const vehicleNewPageCloseButton = document.getElementById("vehicleNewPageCloseButton");
 
 openButton.addEventListener("click", () => {
     overlay.style.display = "block";
@@ -16,4 +20,12 @@ openButton.addEventListener("click", () => {
 closeButton.addEventListener("click", () => {
     overlay.style.display = "none";
     newPage.style.display = "none";
+});
+vehiclePaneOpenButton.addEventListener("click", () => {
+    vehicleOverlay.style.display = "block";
+    newVehiclePage.style.display = "block";
+});
+vehicleNewPageCloseButton.addEventListener("click", () => {
+    vehicleOverlay.style.display = "none";
+    newVehiclePage.style.display = "none";
 });
